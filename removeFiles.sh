36e80249538228b9dffbc72e75b5e726b4ba7f79
owenhuyn <huynowen@gmail.com>
2014-02-10 21:51:23 -0500

    Removes .extensions from the following directory

---
+++ removeFiles.sh
@@ -0,0 +1,5 @@
+find . -name "*.aux" -type f|xargs rm -f
+find . -name "*.~" -type f|xargs rm -f
+find . -name "*.log" -type f|xargs rm -f
+find . -name "*.bak" -type f|xargs rm -f
+find . -name "*.dvi" -type f|xargs rm -f
